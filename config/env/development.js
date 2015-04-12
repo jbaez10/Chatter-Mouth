7489e14d9302c97fd3519e5a1e0a6b3178366a6e
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-04-12 03:20:10 -0400

    minor changes for file name

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
 	db: 'mongodb://localhost/meanscoket-dev',
 	app: {
-		title: 'MEANScoket - Development Environment'
+		title: 'Chatter Box 2.0'
 	},
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
