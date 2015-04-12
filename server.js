3674a7725ce0fbc3b5728a8508b30bb462759f16
Jackeline Baez <baez.jackeline@knights.ucf.edu>
2015-04-12 03:11:09 -0400

    Added js
    added the code that allows the chat to work, but put it in as a comment
    bc it will mess up the server code.

--- server.js
+++ server.js
@@ -7,7 +7,31 @@ var init = require('./config/init')(),
 	mongoose = require('mongoose'),
 	chalk = require('chalk');
 
-	
+
+
+/* HOW TO PUT THIS INTO THE FILE.
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+*/
+
+
 
 /**
  * Main application entry file.
